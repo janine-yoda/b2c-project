2b82d9f1e7aa61e1a6d19bb4c20a0e225ba4a1eb
janine <janine@marianes-air>
2020-01-27 22:59:05 +0800

    add debit

--- Scripts/Positive Scenario/manage_balance/Script1580049410632.groovy
+++ Scripts/Positive Scenario/manage_balance/Script1580049410632.groovy
@@ -15,43 +15,84 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 
-txt_balance = WebUI.getText(findTestObject('Agent List/my_balance'))
+//store the current balance of agent/player
+bal = WebUI.getText(findTestObject('Agent List/my_balance'))
 
-WebUI.setText(findTestObject(null), '1000')
-
-WebUI.setText(findTestObject(null), 'Transfer 1000 through manage balance.')
-
-WebUI.click(findTestObject(null))
-
-WebUI.verifyElementPresent(findTestObject(null), 3)
-
-WebUI.click(findTestObject(null))
-
-WebUI.click(findTestObject(null))
-
-float f_bal = Float.parseFloat(txt_bal)
+//convert stored balance to float
+float f_bal = Float.parseFloat(bal)
 
 System.out.println(f_bal)
 
-bal_credit = (f_bal + 1000.00)
-
-new_txt_bal = WebUI.getText(findTestObject('Agent List/my_balance'))
-
-float new_f_bal = Float.parseFloat(txt_bal)
-
-WebUI.verifyEqual(bal_credit, f_new_bal)
-
+//input the amount to be transferred
 WebUI.setText(findTestObject(null), '1000')
 
+//add note
+WebUI.setText(findTestObject(null), 'Transfer 1000 through manage balance.')
+
+//click submit
 WebUI.click(findTestObject(null))
 
+//verify if success message is present
+WebUI.verifyElementPresent(findTestObject(null), 3)
+
+//click action button
+WebUI.click(findTestObject(null))
+
+//click manage balance
+WebUI.click(findTestObject(null))
+
+//add input balance to stored balance 
+expected_afterCredit_bal = (f_bal + 1000.00)
+
+//store the new balance of agent/player
+afterCredit_bal = WebUI.getText(findTestObject('Agent List/my_balance'))
+
+//convert to float the stored string
+float f_afterCredit_bal = Float.parseFloat(afterCredit_bal)
+
+//compare the stored value and the computed expected value (CREDIT)
+res = WebUI.verifyEqual(f_afterCredit_bal, expected_afterCredit_bal)
+
+//check if equal
+if (res == true) {
+	println ("New balance is correct!");
+}else{
+	println ("New balance is incorrect!");
+}
+
+//input the amount to be deducted
+WebUI.setText(findTestObject(null), '1000')
+
+//check debit
+WebUI.click(findTestObject(null))
+
+//input note
 WebUI.setText(findTestObject(null), 'Deduct 1000 through manage balance.')
 
+//click submit
 WebUI.click(findTestObject(null))
 
-WebUI.verifyElementPresent(findTestObject(null), 0)
+//verify if success message is present
+WebUI.verifyElementPresent(findTestObject(null), 3)
 
+//click action button
 WebUI.click(findTestObject(null))
 
+//click manage balance
 WebUI.click(findTestObject(null))
 
+//store current balance after debit
+afterDebit_bal = WebUI.getText(findTestObject('Agent List/my_balance'))
+
+//subtract the amount deducted from new balance
+expected_afterDebit_bal = (f_afterCredit_bal - 1000.00)
+
+//compare the stored value and the computed expected value (DEBIT)
+res_ii = WebUI.verifyEqual(afterDebit_bal, expected_afterDebit_bal)
+
+//check if equal
+if (res_ii == true) {
+	println ("New balance is correct!");
+}else{
+	println ("New balance is incorrect!");
+}
